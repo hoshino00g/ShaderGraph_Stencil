ac04d730fe3beb14772679bf7eb89e52cff40d53
hoshino00g <hoshiro1998@icloud.com>
2020-12-25 18:00:31 +0900

    FirstCommitStencil

---
+++ Assets/Scripts/CameraStackController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(Camera))]
+public class CameraStackController : MonoBehaviour
+{
+    Camera baseCamera = null;
+    float elapsedTime = 0f;
+
+    void Start()
+    {
+        baseCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        var cameraData = baseCamera.GetUniversalAdditionalCameraData();
+        elapsedTime += Time.deltaTime;
+        Debug.Log(cameraData.cameraStack);//ここは一個
+        if (elapsedTime > 5f)
+        {
+            if (cameraData.cameraStack.Count >= 1)
+            {
+                cameraData.cameraStack.Clear();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SaturationControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class SaturationControl : MonoBehaviour
+{
+    [SerializeField] Material material = null;
+
+    MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = new Material(material);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var saturation = (Mathf.Sin(Time.time) + 1f) / 2f;
+        meshRenderer.material.SetFloat("_Saturation", saturation);//ここでBlackboardに指定した値を入力する。
+        
+    }
+}
